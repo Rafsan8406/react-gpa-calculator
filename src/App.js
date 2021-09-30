d18df911b8bbd96b6c46da83a0cce7014c35167f
Rafsan8406 <pridesys.8406@gmail.com>
2021-09-30 17:32:57 +0600

    react gpa calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import Course from './Course';
+import { useState } from 'react';
 
 function App() {
+    const totalCredit = 0;
+    const totalGrade = 0;
+    const courses=[];
+    const [courseItem, setCourseItem] = useState(courses);
+
+
+    const onRemove = (id) =>{
+      const updateCourse = courseItem.filter((item)=> item.id !== id);
+      setCourseItem(updateCourse); 
+    }
+    const onAdd = () =>{
+      const numberofCourse = courseItem.length+1;
+      const courseObj = {'id':numberofCourse, 'credit': 4.0, 'grade': 3};
+      const newCourseArray = [...courseItem, courseObj]
+      setCourseItem(newCourseArray);
+    }
+    const handleCreditChange = (id) =>{
+
+    }
+    const handleGradeChange = (id) =>{
+      
+    }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {
+          courseItem.map(item =>{
+            return(
+              <div>
+            
+            <div>
+            <select  onChange={()=>handleCreditChange(item.id)}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>
+            <select  onChange={()=>handleGradeChange(item.id)}>
+            <option value="4">A+</option>
+            <option value="3.75">A</option>
+            <option value="3.50">A-</option>
+            <option value="3.25">B+</option>
+            <option value="3">B</option>
+            <option value="2.75">B-</option>
+            <option value="2.50">C+</option>
+            <option value="2.25">C</option>
+            <option value="2">D</option>
+            </select>
+            <button onClick={()=>onRemove(item.id)}>Remove</button>
+            </div>
+           
+             </div>
+             )
+            
+          })
+          
+        }
+         <button onClick={()=>onAdd()}>AddCourse</button>
+        <button onClick={()=> setCourseItem([])}>Clear</button>
+        <button >Generate Result</button>
+      </div>
+     
+     
     </div>
   );
 }
---
+++ src/Course.js
@@ -0,0 +1,24 @@
+function Course(){
+    return(
+        <div>
+            <select>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>
+            <select>
+            <option value="4">A+</option>
+            <option value="3.75">A</option>
+            <option value="3.50">A-</option>
+            <option value="3.25">B+</option>
+            <option value="3">B</option>
+            <option value="2.75">B-</option>
+            <option value="2.50">C+</option>
+            <option value="2.25">C</option>
+            <option value="2">D</option>
+            </select>
+        </div>
+    )
+}
+export default Course;
